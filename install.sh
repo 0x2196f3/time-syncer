25f1e034f97d7a0411cdca4eb94c3a1e2f411c5d
user0 <user0@private.com>
2024-06-08 19:32:16 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@
 read -p "Enter the server URL: " SERVER_URL
 
 # Add the line to crontab
-(crontab -l ; echo "0 0 * * * curl -s $SERVER_URL | xargs -I {} date -s {}") | crontab -
+(crontab -l ; echo "0 0 * * * curl -s $SERVER_URL | xargs -I {} date -s @{}") | crontab -
 
 # Verify that the line has been added to crontab
 crontab -l | grep curl
 
-curl -s $SERVER_URL | xargs -I {} date -s {}
+# Sync time now
+curl -s $SERVER_URL | xargs -I {} date -s @{}
