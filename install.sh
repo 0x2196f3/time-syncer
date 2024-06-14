81198ce9f8f23832e1d0435aa486a8ea967441c6
0x2196f3 <0x2196f3@gmail.com>
2024-06-14 01:54:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Prompt the user for the server URL
-read -p "Enter the server URL: " SERVER_URL
+# Check if a server URL is provided as an argument
+if [ -z "$1" ]; then
+    read -p "Enter the server URL: " SERVER_URL
+else
+    SERVER_URL=$1
+fi
 
 # Add the line to crontab
 (crontab -l ; echo "0 0 * * * curl -s $SERVER_URL | xargs -I {} date -s @{}") | crontab -
