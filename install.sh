fbdf6d1613d2f9be0dc4de2d2190524be17a3f43
0x2196f3 <0x2196f3@gmail.com>
2024-06-14 02:04:50 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ fi
 
 # Add the line to crontab
 (crontab -l ; echo "0 0 * * * curl -s $SERVER_URL | xargs -I {} date -s @{}") | crontab -
+(crontab -l ; echo "@reboot curl -s $SERVER_URL | xargs -I {} date -s @{}") | crontab -
 
 # Verify that the line has been added to crontab
 crontab -l | grep curl
