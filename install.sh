b4e9e7c3a3a477b4f5bc7678cc767abc4cb08daf
0x2196f3 <0x2196f3@gmail.com>
2024-06-19 05:33:01 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,13 @@ fi
 # Add the line to crontab
 (crontab -l ; echo "0 0 * * * curl -s $SERVER_URL | xargs -I {} date -s @{}") | crontab -
 # (crontab -l ; echo "@reboot nohup sleep 120 && curl -s $SERVER_URL | xargs -I {} date -s @{} &") | crontab -
-(crontab -l ; echo "@reboot nohup bash -c \"sleep 120 && curl -s \$SERVER_URL | xargs -I {} date -s @{}\" &") | crontab -
+
+CRON_CMD="@reboot nohup bash -c \"sleep 120 && curl -s " 
+CRON_CMD+=$SERVER_URL
+CRON_CMD+=" | xargs -I {} date -s @{}\" &"
+
+(crontab -l ; echo "$CRON_CMD") | crontab -
+# (crontab -l ; echo "@reboot nohup bash -c \"sleep 120 && curl -s \$SERVER_URL | xargs -I {} date -s @{}\" &") | crontab -
 
 
 # Verify that the line has been added to crontab
